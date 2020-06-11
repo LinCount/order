8c91a2d2ff18f77ec17d77ee458264ac5b5da1ef
mwy <1517955281@qq.com>
2020-06-11 22:58:08 +0800

    莫-商店ID查所有菜

---
+++ src/main/java/com/example/order/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.order.controller;
+
+import com.example.order.Api.Response;
+import com.example.order.Api.ResponseResult;
+import com.example.order.entity.Product;
+import com.example.order.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping(value = "/{s_id}")
+    public ResponseResult<List<Product>> product(@PathVariable(name = "s_id") String s_id){
+        List<Product> product = productService.findProductByS_id(s_id);
+        return Response.makeRsp(200,"菜品查询成功",product);
+    }
+}
